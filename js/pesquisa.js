4ce0b49283907242ebb20c4c5f803e1ff9bcd347
Lauro Santana Silva <bestlaurobr@gmail.com>
2022-08-12 12:34:23 -0300

    dvddvbd

--- js/pesquisa.js
+++ js/pesquisa.js
@@ -5,23 +5,32 @@ const lista_buscada = document.getElementsByClassName("lista_buscada");
 buscador.addEventListener("input", function(){
     const xhttp = new XMLHttpRequest();
     xhttp.onload = function() {
-        //console.log(this.responseText);
+        let conteudos = this.responseText.split('\n')
+        conteudos.forEach(conteudo => {
+            if(buscador.value.match(conteudo)){
+                const lista_buscada = document.getElementById("lista_buscada");
+                lista_buscada.className = "dropdown-menu show";
+                lista_buscada.innerHTML = '<a class="dropdown-item linkBusca" href="#">Piso Cinza Polido</a>';
+            }
+        });
         if(buscador.value != ""){
+            const lista_buscada = document.getElementById("lista_buscada");
+            lista_buscada.className = "dropdown-menu show";
+            lista_buscada.innerHTML = '<a class="dropdown-item linkBusca show" href="#">Piso Cinza Polido</a>';
+        }
+        /*if(buscador.value != ""){
             conteudo_buscado.className = "show";
             for(let i = 0; i < lista_buscada.length; i++){
                 lista_buscada[i].className = "dropdown-menu lista_buscada show";
                 lista_buscada[i].style = "display: flex;";
             }
-        }
+        }*/
       }
-    xhttp.open("GET", "./txt/teste.txt");
+    xhttp.open("GET", "./txt/conteudos.txt");
     xhttp.send();
     buscador.value;
     if(buscador.value == ""){
-        conteudo_buscado.className = "";
-        for(let i = 0; i < lista_buscada.length; i++){
-            lista_buscada[i].className = "dropdown-menu lista_buscada";
-            lista_buscada[i].style = "";
-        }
+        lista_buscada.innerHTML = "";
+        lista_buscada.className = "dropdown-menu";
     }
 })
