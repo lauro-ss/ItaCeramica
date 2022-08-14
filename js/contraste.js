599ecf25b00461c33ad7d41c72d97d1978fc58eb
Lauro Santana Silva <bestlaurobr@gmail.com>
2022-08-14 19:08:26 -0300

    1

--- js/contraste.js
+++ js/contraste.js
@@ -12,16 +12,31 @@ botaoContraste.addEventListener("click", () => {
     }
 
     document.body.classList.toggle('dark');
-    document.getElementsByTagName("main")[0].classList.toggle('dark');
-    document.getElementById('mensagem_footer').classList.toggle('dark');
-    document.getElementById('atendimento').classList.toggle('dark');
-    
+    /*index*/
+    if(document.getElementsByTagName("main")[0])
+        document.getElementsByTagName("main")[0].classList.toggle("dark");
 
-    let vetorClasses = document.getElementsByClassName("link_produtos");
+    document.getElementById("mensagem_footer").classList.toggle("dark");
+    document.getElementById("atendimento").classList.toggle("dark");
+    /*produto*/
+    if(document.getElementById("infor_produto"))
+        document.getElementById("infor_produto").classList.toggle("dark");
+    if(document.getElementById("text_centrado"))
+        document.getElementById("text_centrado").classList.toggle("dark");
+    if(document.getElementById("label_quantidade"))
+        document.getElementById("label_quantidade").classList.toggle("dark");
+    
+    /*formulario*/
+    let vetorClasses = document.getElementsByClassName("contraste-c")
     for(let i = 0; i < vetorClasses.length; i++ )
-        vetorClasses[i].classList.toggle('dark');
+        vetorClasses[i].classList.toggle("dark");
+        
+    /*footer*/
+    vetorClasses = document.getElementsByClassName("link_produtos");
+    for(let i = 0; i < vetorClasses.length; i++ )
+        vetorClasses[i].classList.toggle("dark");
     
     vetorClasses = document.getElementsByClassName("redes_social_link");
     for(let i = 0; i < vetorClasses.length; i++ )
-        vetorClasses[i].classList.toggle('dark');
+        vetorClasses[i].classList.toggle("dark");
 })
